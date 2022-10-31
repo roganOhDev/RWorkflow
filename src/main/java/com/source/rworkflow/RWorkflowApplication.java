14fbd639135d609867ce0af12ecca3c01db5533f
roganOhDev <fdscbjdcnhd@gmail.com>
2022-10-31 23:08:42 +0900

    init spring

---
+++ src/main/java/com/source/rworkflow/RWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.source.rworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RWorkflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RWorkflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/source/rworkflow/RWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.source.rworkflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RWorkflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
