4b978b81b89bb569cb7d44455e0b94fc418daa83
roganOhDev <fdscbjdcnhd@gmail.com>
2022-11-01 00:16:39 +0900

    init flyway & metadata DB

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,5 @@
+create table sample_entity(
+                              id   bigint auto_increment,
+                              name varchar(255),
+                              primary key (id)
+);
