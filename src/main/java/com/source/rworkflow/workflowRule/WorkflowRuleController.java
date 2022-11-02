94638bea073e3e79d9d5b2719134c2942e430cff
roganOh <rogan@chequer.io>
2022-11-03 01:59:46 +0900

    fix delete

--- src/main/java/com/source/rworkflow/workflowRule/WorkflowRuleController.java
+++ src/main/java/com/source/rworkflow/workflowRule/WorkflowRuleController.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -36,7 +37,7 @@ public class WorkflowRuleController {
     }
 
     @DeleteMapping(path = "/{id}")
-    public WorkflowRuleDto.Delete.Response delete(final @RequestParam Long id, @RequestHeader(HEADER_KEY) String userToken) {
+    public WorkflowRuleDto.Delete.Response delete(final @PathVariable Long id, @RequestHeader(HEADER_KEY) String userToken) {
         final var sessionUserId = userTokenService.findUserId(userToken);
 
         return transferService.delete(id, sessionUserId);
